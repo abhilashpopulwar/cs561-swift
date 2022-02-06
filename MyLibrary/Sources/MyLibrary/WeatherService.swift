39cb8ea44622346a7b26458374be6459e40b1925
Abhilash Populwar <abhilashpopulwar@gmail.com>
2022-02-05 23:41:35 -0800

    Add files via upload

--- MyLibrary/Sources/MyLibrary/WeatherService.swift
+++ MyLibrary/Sources/MyLibrary/WeatherService.swift
@@ -1,4 +1,5 @@
 import Alamofire
+import Combine
 
 public protocol WeatherService {
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void)
@@ -6,20 +7,20 @@ public protocol WeatherService {
 }
 
 class WeatherServiceImpl: WeatherService {
-    let url = "http://44.202.0.196:3000/v1/weather"
-    let parameters = ["username": "bar", "password": "some_val"]
+    let url = "http://34.224.222.186:3000/v1/weather"
+    let parameters = ["username": "Abhi", "password": "password"]
     func getTemperature(completion: @escaping (_ response: Result<Int /* Temperature */, Error>) -> Void) {
         
-        AF.request("http://44.202.0.196:3000/v1/auth", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseDecodable(of: Auth.self) { response in
+        AF.request("http://34.224.222.186:3000/v1/auth", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseDecodable(of: Auth.self) { response in
             switch response.result {
             case let .success(auth):
-                print(auth)
                 let token = auth.access_token
+                print(token)
                 let headers: HTTPHeaders = [
                     "Authorization": "Bearer " + token,
                     "Accept": "application/json"
                 ]
-                AF.request("http://44.202.0.196:3000/v1/weather", method: .get, headers: headers).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
+                AF.request("http://34.224.222.186:3000/v1/weather", method: .get, headers: headers).validate(statusCode: 200..<300).responseDecodable(of: Weather.self) { response in
                     switch response.result {
                     case let .success(weather):
                         let temperature = weather.main.temp
@@ -37,19 +38,24 @@ class WeatherServiceImpl: WeatherService {
             }
     func getGreeting(completion: @escaping (_ response: Result<String /* Temperature */, Error>) -> Void) {
         
-        AF.request("http://44.202.0.196:3000/v1/auth", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseDecodable(of: Auth.self) { response in
+        AF.request("http://34.224.222.186:3000/v1/auth", method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: nil).responseDecodable(of: Auth.self) { response in
             switch response.result {
             case let .success(auth):
                 print(auth)
                 let token = auth.access_token
+                print("token = "+token)
                 let headers: HTTPHeaders = [
-                    "Authorization": "Basic " + token,
+                    "Authorization": "Bearer " + token,
                     "Accept": "application/json"
                 ]
-                AF.request("http://44.202.0.196:3000/v1/hello", method: .get, headers: headers).validate(statusCode: 200..<300).responseDecodable(of: Greeting.self) { response in
+                print("headers = ")
+                print(headers)
+                AF.request("http://34.224.222.186:3000/v1/hello", method: .get, headers: headers).validate(statusCode: 200..<300).responseDecodable(of: Greeting.self) { response in
                     switch response.result {
-                    case let .success(greeting):
-                        let message = greeting.message
+                    case let .success(msg):
+                        print("greeting = ")
+                        print(msg)
+                        let message = msg.greeting
                         print(message)
                         completion(.success(message))
 
@@ -77,5 +83,5 @@ private struct Auth: Decodable {
 }
 
 private struct Greeting: Decodable {
-    let message: String
+    let greeting: String
 }
